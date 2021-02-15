95edc13f7a4329d4f16a18befcc4f4f3000b09a2
FebrianYudhis <killersvteam@gmail.com>
2021-02-15 10:40:01 +0700

    Initial Komit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Auto Downloader</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="assets/image/icon/app.ico" type="image/x-icon">
+</head>
+
+<body>
+
+
+    <link rel="stylesheet" href="assets/js/jquery-3.5.1.slim.min.js">
+    <link rel="stylesheet" href="assets/js/bootstrap.bundle.min.js">
+</body>
+
+</html>
